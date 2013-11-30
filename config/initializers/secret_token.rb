f7daa08b0b66c8571b4319b1c1573f07615db1f4
eddiefisher <eddifisher@bk.ru>
2013-11-30 12:28:12 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ButhubRais::Application.config.secret_key_base = 'f8f518490dc335aef4ee4b26839fe0834f8ba4aa8789310ac567fc96488e47307f89a46c4457b7b8e459e101332f79fe45c63407cf6d2fe87794cfca6c02d0e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ButhubRais::Application.config.session_store :cookie_store, key: '_buthub_rais_session'
